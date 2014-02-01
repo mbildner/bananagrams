b8dd1e689def90e0aca996a5524b6f63e5ccdab2
Moshe Bildner <moshe.bildner@gmail.com>
2014-02-01 12:48:14 -0500

    adds player class

--- Bananagrams.py
+++ Bananagrams.py
@@ -1,5 +1,13 @@
 from random import shuffle
 
+
+class GamePlayer(object):
+	"""Holds game state for individual players and manages their state transitions"""
+	def __init__(self, name):
+		self.name = name
+		self.tiles = []
+
+
 class GameModel(object):
 	"""Holds all game state and manages state transitions for the bananagrams game"""
 
@@ -9,33 +17,35 @@ class GameModel(object):
 	"k": 2,	"l": 5,	"m": 3,	"n": 8,	"o": 11,
 	"p": 3,	"q": 2,	"r": 9,	"s": 6,	"t": 9,
 	"u": 6,	"v": 3,	"w": 3,	"x": 2,	"y": 3,
-	"z": 2}
+	"z": 2
+	}
 
 	def __init__(self, players):
-		self.tiles = self.create_tile_bucket(self)
+		self.tiles = self.create_tile_bucket()
 		self.players = players
-
 		self.number_of_players = len(self.players)
 
-	def create_tile_bucket(self, number_of_tiles):
+		self.distribute_tiles(self.players)
+
+	def create_tile_bucket(self):
 		nested_tile_lists = [letter * distribution for letter, distribution in self.letter_distributions.items()]
 		tiles = [tile for sublist in nested_tile_lists for tile in sublist]
 		shuffle(tiles)
 		return tiles
 		
-	def give_player_tile(self, tiles, player):
-		pass
+	def give_player_tiles(self, tiles, player):
+		player.tiles += self.tiles[0:tiles]
 		
-	def distribute_tiles(self, tiles, players):
+	def distribute_tiles(self, players):
 		if self.number_of_players < 5:
 			tiles_per_person = 21
-		else if self.number_of_players < 7:
+		elif self.number_of_players < 7:
 			tiles_per_person = 15
-		else if self.number_of_players > 6:
+		elif self.number_of_players > 6:
 			tiles_per_person = 11
 
 		for player in players:
-			self.give_player_tile(tiles, player)
+			self.give_player_tiles(tiles_per_person, player)
 			
 
 
@@ -43,10 +53,9 @@ class GameModel(object):
 
 
 
-class GamePlayer(object):
-	"""Holds game state for individual players and manages their state transitions"""
-	def __init__(self, name, tiles):
-		self.name = name
-		self.tiles = tiles
+
+
+
+
 
 
--- bananagrams_unittests.py
+++ bananagrams_unittests.py
@@ -1,5 +1,5 @@
 import unittest
-from Bananagrams import GameModel
+from Bananagrams import GameModel, GamePlayer
 
 from copy import deepcopy
 
@@ -7,7 +7,12 @@ from copy import deepcopy
 
 class TestInitializeGameFunction(unittest.TestCase):
 	def setUp(self):
-		self.game_model = GameModel(players=2)
+	 	player1 = GamePlayer('player1')
+	 	player2 = GamePlayer('player2')
+
+	 	players = [player1, player2]
+
+		self.game_model = GameModel(players)
 
 	def test_letter_bucket_creation(self):
 		tiles = self.game_model.tiles
@@ -17,7 +22,6 @@ class TestInitializeGameFunction(unittest.TestCase):
 		self.assertNotEqual(cloned_tiles, tiles)
 
 	
-
 	def test_player_setups(self):
 		players = self.game_model.players
 		number_of_players = len(players)
@@ -25,19 +29,20 @@ class TestInitializeGameFunction(unittest.TestCase):
 		if number_of_players < 5:
 			tiles_per_person = 21
 
-		else if number_of_players < 7:
+		elif number_of_players < 7:
 			tiles_per_person = 15
 
-		else if number_of_players > 6:
+		elif number_of_players > 6:
 			tiles_per_person = 11
 
 		for player in self.game_model.players:
-			self.assertEqual()
+			self.assertEqual(len(player.tiles), tiles_per_person)
 
 
 
 
 if __name__ == '__main__':
 	unittest.main()
+
 
 
