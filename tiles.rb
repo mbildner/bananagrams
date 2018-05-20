4b34c8c5c5e80fbe82be1243ae7103461856da51
Moshe Bildner <moshe.bildner@gmail.com>
2018-05-20 11:14:32 -0400

    add IO*HEAVY cache to pearson dictionary

--- tiles.rb
+++ tiles.rb
@@ -1,19 +1,68 @@
 require 'json'
 require 'set'
+require 'rest-client'
 
-class Dictionary
-  def initialize
+PEARSON_APP_ID=ENV.fetch('PEARSON_APP_ID')
+PEARSON_API_KEY=ENV.fetch('PEARSON_API_KEY')
+
+class PearsonDictionary
+  CACHE_FILE_NAME = '.dict.json'
+  attr_reader :app_id, :api_key
+
+  def initialize(app_id, api_key)
+    @app_id = app_id
+    @api_key = api_key
+
+    File.write(CACHE_FILE_NAME, "{}") unless File.exist?(CACHE_FILE_NAME)
+  end
+
+  def read_cache
+    JSON.parse(File.read(CACHE_FILE_NAME))
+  end
+
+  def save_cache(cache)
+    File.write(CACHE_FILE_NAME, cache.to_json)
+  end
+
+  def headers
+    @_headers ||= {
+      'app_id' => app_id,
+      'app_key' => api_key
+    }
+  end
+
+  def url(word)
+    "http://api.pearson.com/v2/dictionaries/ldoce5/entries?headword=#{word}"
+  end
+
+  def check_api(word)
+    response = RestClient.get(
+      url(word),
+      headers
+    )
+
+    results = JSON.parse(response)
+
+    !results.empty?
   end
 
   def confirm(word)
-    [
-      'apple',
-      'eat',
-      'eats',
-      'reads'
-    ].include? word
+    cache = read_cache
+
+    if cache.include?(word)
+      return cache[word]
+    end
+
+    result = check_api(word)
+
+    cache[word] = result
+
+    save_cache(cache)
+
+    result
   end
 end
+
 
 class WordFinder
   attr_reader :grid
@@ -97,7 +146,7 @@ class Board
 
   attr_reader :grid, :dictionary
 
-  def initialize(grid, dictionary=Dictionary.new)
+  def initialize(grid, dictionary=PearsonDictionary.new(PEARSON_APP_ID, PEARSON_API_KEY))
     @grid = grid
     @dictionary = dictionary
   end
